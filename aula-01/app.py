2e20f5f0ced7369acb64d7b7d9d27bb08b9da72a
Pedro <pedrosantos.12ds@gmail.com>
2025-02-18 11:44:11 -0300

    18/02/2025

--- aula-01/app.py
+++ aula-01/app.py
@@ -1,15 +1,24 @@
 #Importanto o flask
-from flask import Flask
+from flask import Flask, render_template
 
 #Carregando o flask na variavel app
-app = Flask (__name__)
+app = Flask (__name__,template_folder='views')
 
 #Criando a primeira rota do site
 @app.route('/')
 
 #Criando função do python
 def home():
-    return '<h1>Bem-vindo ao meu primeiro site em Flask!</h1>'
+    return render_template('index.html')
+
+@app.route ('/games')
+def games ():
+    titulo = 'CS-GO'
+    ano = 2012
+    categoria = 'FPS Online'
+    jogadores=['Midna','Vt','Leaf','Quemario','Trop','Aspax','maxxdiego']
+    jogos=['GTA V','Valorant','Elden Ring','Sekiro','Free Fire','Mad Max','Dying Light']
+    return render_template("games.html",titulo= titulo,ano=ano,categoria=categoria,jogadores=jogadores,jogos=jogos)
 
 #Iniciando o servidor no localhost, porta 500, modo de depuração ativado
 if __name__ == '__main__':
